3e3f3a61cf367f33efebc4b40c31adfb16f9c30c
BLACKPANTHER11544 <vaibhavgiri460@gmail.com>
2023-09-05 21:25:57 +0530

    new updates

--- backend/controllers/OrderController.js
+++ backend/controllers/OrderController.js
@@ -79,7 +79,7 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
       email_address: req.body.payer.email_address,
     };
     const updateOrder = await order.save();
-    res.status(200).json(updatedOrder);
+    res.status(200).json(updateOrder);
   } else {
     res.status(404);
     throw new Error("Order not found");
